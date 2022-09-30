a6a488615c423a24e515e084067aac18195a582d
Paulo <metasoft.brasil@gmail.com>
2022-09-30 15:11:01 -0300

    add grid list

--- src/App.js
+++ src/App.js
@@ -7,32 +7,42 @@ import Paper from '@mui/material/Paper';
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
   padding: theme.spacing(1),
+  margin: theme.spacing(1),
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
 
 function TaskList() {
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-                <Grid xs={12}>
-                    <Item>
-                        <FormControl sx={{ width: '25ch' }}>
-                            <TextField id="task"
-                                label="Describe a task"
-                                margin="normal"
-                                variant="outlined" />
-                            <Button variant="contained">Add Task</Button>
-                        </FormControl>
-                    </Item>
-                </Grid>
-            </Grid>
-        </Box>
+        <Container maxWidth="lg">
+            <Item>
+                <FormControl sx={{ width: 1 }}>
+                    <TextField id="task"
+                        label="Describe a task"
+                        margin="normal"
+                        variant="outlined" />
+                    <Button variant="contained">Add Task</Button>
+                </FormControl>
+            </Item>
+            <Item>
+                <Box sx={{ flexGrow: 1 }}>
+                    <Grid container spacing={2}>
+                        <Grid xs={12}>
+                            <p>This is a new task</p>
+                        </Grid>
+                        <Grid xs={12}>
+                            <p>This is a new task</p>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Item>
+        </Container>
     );
 }
 
