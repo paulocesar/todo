41b09206017c4149cb077d4e5d216e743e2a45c3
Paulo <metasoft.brasil@gmail.com>
2022-12-13 12:16:10 -0300

    add more layout

--- src/App.js
+++ src/App.js
@@ -1,53 +1,80 @@
-import * as React from 'react';
+import { useEffect, useState } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline, Typography } from "@mui/material";
+import { TextField, Button, Container, Grid } from "@mui/material";
+import moment from "moment";
 
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Unstable_Grid2';
-import Paper from '@mui/material/Paper';
-import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
+const availableThemes = {
+  default: createTheme(),
+  dark: createTheme({ palette: { mode: "dark" } })
+};
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  margin: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+function useTime() {
+  const [time, setTime] = useState(moment());
 
-function TaskList() {
-    return (
-        <Container maxWidth="lg">
-            <Item>
-                <FormControl sx={{ width: 1 }}>
-                    <TextField id="task"
-                        label="Describe a task"
-                        margin="normal"
-                        variant="outlined" />
-                    <Button variant="contained">Add Task</Button>
-                </FormControl>
-            </Item>
-            <Item>
-                <Box sx={{ flexGrow: 1 }}>
-                    <Grid container spacing={2}>
-                        <Grid xs={12}>
-                            <p>This is a new task</p>
-                        </Grid>
-                        <Grid xs={12}>
-                            <p>This is a new task</p>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Item>
-        </Container>
-    );
+  function tick() {
+    setTime(moment());
+  }
+
+  useEffect(() => {
+    const tickId = setInterval(tick, 1000);
+    return () => clearInterval(tickId);
+  });
+
+  return [time];
 }
 
-function App() {
-  return <TaskList />;
+function Clock(props) {
+  const [time] = useTime();
+  const defaultFormat = "MM/DD/YYYY HH:mm:ss";
+
+  return (
+    <Typography variant="p">
+      {time.format(props.format || defaultFormat)}
+    </Typography>
+  );
 }
 
-export default App;
+export default function App() {
+  const [currentTheme, setCurrentTheme] = useState(availableThemes.default);
+
+  return (
+    <div className="App">
+      <ThemeProvider theme={currentTheme}>
+        <CssBaseline />
+          <Grid container spacing={1} sx={{p: 1}} >
+            <Grid item xs={6}>
+              <Typography variant="h2">TODO</Typography>
+              <Clock />
+            </Grid>
+          </Grid>
+          <Grid container spacing={1} sx={{p: 1}} >
+            <Grid item xs={9}>
+              <TextField
+                sx={{ width: 1 }}
+                label="Add task"
+                InputLabelProps={{ shrink: true }}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <Button sx={{ p: 2, width: 1 }} variant="contained">
+                Add
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container spacing={1} sx={{p: 1}} >
+            <Grid item xs={4}>
+              <Typography variant="h5">Next</Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="h5">In Progress</Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="h5">Done</Typography>
+            </Grid>
+          </Grid>
+      </ThemeProvider>
+    </div>
+  );
+}
+
